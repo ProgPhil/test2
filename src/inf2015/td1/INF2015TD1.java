ce485014b9e52d809cc6e71a3b8fedb4683eb3e7
plavoie13@hotmail.com <plavoie13@hotmail.com>
2017-09-18 15:47:47 -0400

    initial git commit

---
+++ src/inf2015/td1/INF2015TD1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inf2015.td1;
+
+/*
+ *
+ * @author Philippe
+ */
+public class INF2015TD1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("Bonjours "+args[0]+" "+args[1]);
+        
+        
+    }
+    
+}
